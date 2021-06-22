7c2879e506d18d09f0c96799911583f157d21e0a
afshanafalza <71531125+afshanafalza@users.noreply.github.com>
2021-06-21 22:33:51 -0400

    checking duplications

--- src/component/todoList/index.js
+++ src/component/todoList/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid'
 import TaskCard from '../taskCard'
+import Alert from '@material-ui/lab/Alert';
 
 
 // CSS Style
@@ -39,9 +40,41 @@ class TodoList extends React.Component {
 
   // Function to add created task to list of tasks
   addTask = (task) => {
-    console.log('task',task);
-    console.log('list', this.state.list);
-    this.state.list.push(task)
+    var duplicate = false;
+    // If the list is empty
+    if(this.state.list.length===0) {
+      console.log('task',task);
+      console.log('list', this.state.list);
+      this.state.list.push(task)
+    }
+    else { // List contents exist
+      // Iterate through the list
+      for(var i=0; i<this.state.list.length; i++) {
+        if(this.state.list[i].title===task.title) { // If current title equals any existing titles
+          duplicate=true;
+          console.log(duplicate);
+          break;
+          // Display message that says this title already exists
+        }
+        else {
+          duplicate=false;
+          console.log(duplicate);
+        }
+      }
+      if(duplicate===true) {
+        console.log('this exists');
+      }
+      else {
+        console.log('this doesnt exist');
+        this.state.list.push(task)
+      }
+    }
+
+
+    // Start iterating through task and comp
+    // If you find one that is equal, end the loop and say you found a duplicate
+    // If not, then display as normal
+
     //I dont fully understand setState so I am just gonna push the task to the list - mieraf
     // this.setState(state => {
     //   const list = state.list.concat(task);
@@ -66,6 +99,8 @@ class TodoList extends React.Component {
       </Grid>
     );
   }
+
+  
 }
 
 
